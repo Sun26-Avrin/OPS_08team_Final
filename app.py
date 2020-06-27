a1642a93debddbcf9d53cdf65659ff1f6cb1d9b2
sa02045 <sa02045@naver.com>
2020-06-27 22:58:26 +0900

    tf.html cosine.html 팝업창

--- app.py
+++ app.py
@@ -2,14 +2,11 @@
 import sys,requests, time,numpy, math
 from bs4 import BeautifulSoup
 from elasticsearch import Elasticsearch
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask,render_template,request
 from nltk import word_tokenize
 
 app = Flask(__name__)
 
-count = 0
 es_host = "127.0.0.1"
 es_port = "9200"
 es = Elasticsearch([{'host':es_host,'port':es_port}],timeout=30)
@@ -17,3 +14,15 @@ es = Elasticsearch([{'host':es_host,'port':es_port}],timeout=30)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/word',method=['GET','POST'])
+def word():
+    return render_template('final.html',value=result)
+
+@app.route('/tf')
+def tf():
+    return render_template('tf.html')
+
+@app.route('/cosine')
+def tf():
+    return render_template('cosine.html')
